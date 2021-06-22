4d66b02eb9377bdc01d96cb02eb4dd2da0afe999
Bhavesh083 <76737905+Bhavesh083@users.noreply.github.com>
2021-06-22 21:14:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors()); 
 app.use(express.json());
  
-mongoose.connect("mongodb+srv://bikerbhavesh:bhaveshbobby@cluster0.x2gaq.mongodb.net/appscheduler?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true }).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
+mongoose.connect("mongodb+srv://*****:******@cluster0.x2gaq.mongodb.net/appscheduler?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true }).then(()=> console.log(" Successfully running on 5000")).catch(error => console.log("Failed to connect",error));
 
 const appointRouter = require('./routes/appschedroute'); 
 //const usersRouter = require('./routes/users'); 
